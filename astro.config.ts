883dcbdf337c9ec26803b1f84e079aa612b90cc2
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2026-01-11 15:36:09 +0200

    feat: about section

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel/static'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://maximilianhagelstam.com',
   output: 'static',
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
