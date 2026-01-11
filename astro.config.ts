86defdc81c76450a4d6ae54bb47ce0a85ee6881b
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2026-01-11 11:15:47 +0200

    feat: setup tailwindcss

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config'
-import vercelStatic from '@astrojs/vercel/static'
+import vercel from '@astrojs/vercel/static'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercelStatic({
+  adapter: vercel({
     webAnalytics: { enabled: true },
   }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
