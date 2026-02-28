6b9c7d3c1e7882f504bd7544b399734710974532
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2026-02-28 11:54:40 +0200

    chore: bump deps

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel'
+import vercel from '@astrojs/vercel/static'
 import tailwindcss from '@tailwindcss/vite'
 import sitemap from '@astrojs/sitemap'
 
