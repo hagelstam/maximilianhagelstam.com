51c64fff916499d1fd2497796e58feadfb6bc548
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2026-03-01 16:12:39 +0200

    chore: upgrade tools

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/static'
+import vercel from '@astrojs/vercel'
 import tailwindcss from '@tailwindcss/vite'
 import sitemap from '@astrojs/sitemap'
 
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/hagelstam/reading
 
-go 1.25.6
+go 1.26.0
 
 require github.com/mmcdole/gofeed v1.3.0
 
